3bbc479ce2d8e6e6df5e28b01ad29b9c90b35258
adi-kiran <adithyakiran1999@gmail.com>
2020-11-30 13:42:43 +0530

    modified print_vulnerabilities to include suggested fix

--- data/print_vulnerabilities.py
+++ data/print_vulnerabilities.py
@@ -7,9 +7,10 @@ prefix = "CVE-codewisdom-ts-"
 for i,j,k in os.walk(path_to_files):
     files_list.extend(k)
 final_res = {}
+upgrades = {}
 for name in files_list:
-    print("\nImage Name : ",name[:-4])
-    print("***********************")
+    # print("\nImage Name : ",name[:-4])
+    # print("***********************")
     f = open(path_to_files+name,'r')
     x = f.readlines()
     f.close()
@@ -25,28 +26,37 @@ for name in files_list:
     f.close()
     cve_set = set()
     package_set = set()
+    suggested_upgrades = {}
     severity = {"CRITICAL":0,"HIGH":0,"MEDIUM":0,"LOW":0}
     vulns = vulns[0]["Vulnerabilities"]
+    f = open("./vulnerability_list/"+name,'w')
     for i in vulns:
         if i["PkgName"] in x:
+            package_set.add(i["PkgName"])
+            if "FixedVersion" in i:
+                if i["PkgName"] in suggested_upgrades:
+                    suggested_upgrades[i["PkgName"]].add(i["FixedVersion"])
+                else:
+                    suggested_upgrades[i["PkgName"]] = set()
+                    suggested_upgrades[i["PkgName"]].add(i["FixedVersion"])
             if i["VulnerabilityID"] not in cve_set:
-                print(i["VulnerabilityID"])
+                print(i["VulnerabilityID"],file=f)
                 cve_set.add(i["VulnerabilityID"])
-                print("Package Name : ",i["PkgName"])
-                package_set.add(i["PkgName"])
-                print("Severity : ",i["Severity"])
+                print("Package Name : ",i["PkgName"],file=f)
+                print("Severity : ",i["Severity"],file=f)
                 if "Title" in i:
-                    print("Title : ",i["Title"])
+                    print("Title : ",i["Title"],file=f)
                 if "Descreption" in i:
-                    print("Descreption : ",i["Description"])
-                print("CVSS : ",i["CVSS"])
-                print("\n................\n")
+                    print("Descreption : ",i["Description"],file=f)
+                print("CVSS : ",i["CVSS"],file=f)
+                print("\n................\n",file=f)
                 severity[i["Severity"]]+=1
-            else:
-                package_set.add(i["PkgName"])
     final_res[name[:-4]]=(severity,package_set)
-    print("------------------------")
-print("\n\n\n\n\n")
+    upgrades[name[:-4]] = suggested_upgrades
+    print("------------------------",file=f)
+    print(package_set,'\n',suggested_upgrades,"\n",severity,file=f)
+    f.close()
+# print("\n\n\n\n\n")
 print("Vulnerability Counts and Vulnerable Packages : \n")
 print(" "*30,"|CRITICAL|","|HIGH|","|MEDIUM|","|LOW|","|TOTAL|","\tPACKAGES",sep="")
 for i,j in final_res.items():
@@ -58,3 +68,11 @@ for i,j in final_res.items():
     print(str(tot).center(5," "),end="\t")
     print(j[1])
     # print("\n---------------------\n")
+print("\n\n\n\n\n")
+print("Possible Upgrades to fix Vulnerabilities : ")
+for i,j in upgrades.items():
+    print(i)
+    print("***************")
+    for a,b in j.items():
+        print(a,"\t",b)
+    print("....................................................\n")
